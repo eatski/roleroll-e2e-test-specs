ae8c6e41cc9f251ad7d2ec2a5dd358a4fb962251
hagaitski <i.haga629@gmail.com>
2022-10-12 03:01:24 +0900

    参加を締め切られたルームには参加できない

--- cypress/e2e/lobby.cy.ts
+++ cypress/e2e/lobby.cy.ts
@@ -2,6 +2,10 @@ const prepareHostRoomCreate = () => {
     cy.visit("http://localhost:8080")
     cy.findByRole("region",{ name: "ルール作成から" }).findByRole("button", { name: "作成" }).click();
     cy.findByRole("heading", {name: "メンバーを集めましょう"});
+    
+}
+
+const reflesh = () => {
     cy.clearLocalStorage();
     cy.reload()
 }
@@ -9,6 +13,7 @@ const prepareHostRoomCreate = () => {
 describe("lobby", () => {
     it("ゲストとしてルームに訪れると参加フォームが表示される",() => {
         prepareHostRoomCreate();
+        reflesh();
         const anyGuestName = "guest";
         cy.findByRole("form", { name: "名前を入力して参加する" }).within(() => {
             cy.findByRole("textbox", { name: "あなたの名前" }).type(anyGuestName);
@@ -19,9 +24,17 @@ describe("lobby", () => {
     })
     it("名前が空欄では参加できない",() => {
         prepareHostRoomCreate();
+        reflesh();
         cy.findByRole("form", { name: "名前を入力して参加する" }).within(() => {
             cy.findByRole("textbox", { name: "あなたの名前" }).clear();
             cy.findByRole("button", { name: "参加" }).should("be.disabled");
         });
+    })
+    it("参加を締め切られたルームには参加できない",() => {
+        prepareHostRoomCreate();
+        cy.findByRole("button",{ name:"締め切る" }).click();
+        reflesh();
+        cy.findByRole("form").should("not.exist");
+        cy.findByText("この部屋は参加を締め切られました。").should("exist");
     })
 })
